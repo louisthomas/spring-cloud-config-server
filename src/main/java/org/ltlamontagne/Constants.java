693a88e457b15721d40e77ab4bfdf8205cd427cd
ltlamontagne <louis-thomas.lamontagne@bellmedia.ca>
2017-02-23 13:45:08 -0500

    Create dev and prod configuration base on JHipster :)

---
+++ src/main/java/org/ltlamontagne/Constants.java
@@ -0,0 +1,8 @@
+package org.ltlamontagne;
+
+
+public final class Constants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+}
--- src/main/java/org/ltlamontagne/SpringCloudConfigServerApplication.java
+++ src/main/java/org/ltlamontagne/SpringCloudConfigServerApplication.java
@@ -1,14 +1,56 @@
 package org.ltlamontagne;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class SpringCloudConfigServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    private static final Logger log = LoggerFactory.getLogger(SpringCloudConfigServerApplication.class);
+
+    private final Environment env;
+
+    public SpringCloudConfigServerApplication(Environment env) {
+        this.env = env;
+    }
+
+    @PostConstruct
+    public void initApplication() {
+        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
+            log.error("You can't run both 'dev' and 'prod' profiles at the same time.");
+        }
+    }
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication application = new SpringApplication(SpringCloudConfigServerApplication.class);
+        DefaultProfileUtil.addDefaultProfile(application);
+        Environment env = application.run(args).getEnvironment();
+        String protocol = "http";
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n\t" +
+                        "External: \t{}://{}:{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
     }
 }
